79a29365c4e32ec9b786f0203be41eb8564ca2d3
SealCHK <kospryshin@gmail.com>
2022-09-26 22:08:32 +0600

    12

--- DB Settings Checker/Form1.Designer.cs
+++ DB Settings Checker/Form1.Designer.cs
@@ -28,12 +28,313 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Порт подключения";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(503, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(503, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Разрешенные IP-адреса";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(503, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(281, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Количество одновременных подключений";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(503, 161);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(216, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Время для аутентификации (сек)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(115, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Шифрование паролей";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(503, 225);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 26);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(115, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(308, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Проверка доступа пользователя к базе данных";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(503, 257);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 26);
+            this.textBox7.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(115, 260);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 19);
+            this.label7.TabIndex = 12;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(503, 289);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 26);
+            this.textBox8.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(115, 292);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 19);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "label8";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(503, 321);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 26);
+            this.textBox9.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(115, 324);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 19);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "label9";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(503, 353);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 26);
+            this.textBox10.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(115, 356);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 19);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "label10";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(503, 385);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 26);
+            this.textBox11.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(115, 388);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 19);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "label11";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(503, 417);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 26);
+            this.textBox12.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(115, 420);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(53, 19);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(54, 21);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(606, 19);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "С помощью данной программы вы можете настроить безопасность базы данных PostgreSQ" +
+    "L";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(426, 41);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "ПРИМЕНИТЬ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(711, 506);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox6;
+        private Label label6;
+        private TextBox textBox7;
+        private Label label7;
+        private TextBox textBox8;
+        private Label label8;
+        private TextBox textBox9;
+        private Label label9;
+        private TextBox textBox10;
+        private Label label10;
+        private TextBox textBox11;
+        private Label label11;
+        private TextBox textBox12;
+        private Label label12;
+        private Label label13;
+        private Button button1;
+        private Button button2;
     }
 }
--- DB Settings Checker/Form1.cs
+++ DB Settings Checker/Form1.cs
@@ -1,3 +1,8 @@
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DB_Settings_Checker
 {
     public partial class Form1 : Form
@@ -6,5 +11,62 @@ namespace DB_Settings_Checker
         {
             InitializeComponent();
         }
+        public class INIManager
+        {
+            //Конструктор, принимающий путь к INI-файлу
+            public INIManager(string aPath)
+            {
+                path = aPath;
+            }
+
+            //Конструктор без аргументов (путь к INI-файлу нужно будет задать отдельно)
+            public INIManager() : this("") { }
+
+            //Возвращает значение из INI-файла (по указанным секции и ключу) 
+            public string GetPrivateString(string aSection, string aKey)
+            {
+                //Для получения значения
+                StringBuilder buffer = new StringBuilder(SIZE);
+
+                //Получить значение в buffer
+                GetPrivateString(aSection, aKey, null, buffer, SIZE, path);
+
+                //Вернуть полученное значение
+                return buffer.ToString();
+            }
+
+            //Пишет значение в INI-файл (по указанным секции и ключу) 
+            public void WritePrivateString(string aSection, string aKey, string aValue)
+            {
+                //Записать значение в INI-файл
+                WritePrivateString(aSection, aKey, aValue, path);
+            }
+
+            //Возвращает или устанавливает путь к INI файлу
+            public string Path { get { return path; } set { path = value; } }
+
+            //Поля класса
+            private const int SIZE = 1024; //Максимальный размер (для чтения значения из файла)
+            private string path = null; //Для хранения пути к INI-файлу
+
+            //Импорт функции GetPrivateProfileString (для чтения значений) из библиотеки kernel32.dll
+            [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+            private static extern int GetPrivateString(string section, string key, string def, StringBuilder buffer, int size, string path);
+
+            //Импорт функции WritePrivateProfileString (для записи значений) из библиотеки kernel32.dll
+            [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
+            private static extern int WritePrivateString(string section, string key, string str, string path);
+        }
+        /*
+        //Создание объекта, для работы с файлом
+        INIManager manager = new INIManager("C:\\my.ini");
+
+        //Получить значение по ключу name из секции main
+        string name = manager.GetPrivateString("main", "name");
+
+        //Записать значение по ключу age в секции main
+        manager.WritePrivateString("main", "age", "21");
+
+        */
     }
 }
