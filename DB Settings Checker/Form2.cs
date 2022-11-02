de08a7f3ffe1786ba2d8336791d7043660e20176
SealCHK <kospryshin@gmail.com>
2022-11-02 22:33:55 +0600

    12

--- DB Settings Checker/Form2.cs
+++ DB Settings Checker/Form2.cs
@@ -19,40 +19,36 @@ namespace DB_Settings_Checker
         {
             InitializeComponent();
         }
-        public string str() 
+        public string str() // получаем строку подключения, это для 1 формы
         {
             return "server=localhost;user=" + textBox1.Text + ";database=mydb;password=" + textBox2.Text + ";port=3306;";
         }
-
-        public string pass()
+        public string pass() //получаем пароль, так же для 1 формы 
         {
             return textBox2.Text;
         }
-
         public void button1_Click(object sender, EventArgs e)
         {
             Form1 newform = new Form1();
             newform.Owner = this;
             //string connStr = "server=localhost;user=" + login +";database=mydb;password=" + pass + ";port=3360;";
-            try
+            try // пробуем подключиться к БД
             {
                 MySqlConnection conn = new MySqlConnection(str());
 
                 conn.Open();
                 conn.Close();
                 this.Hide();
-                newform.Show();
+                newform.Show(); // всё отлично открываем 1 форму для работы с приложением
             }
-
-            catch (Exception)
+            catch (Exception) //ошибка 
             {
                 MessageBox.Show("Неправильные параметры для входа");
             }
         }
-        
         private void textBox2_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter) button1.PerformClick();
+            if (e.KeyCode == Keys.Enter) button1.PerformClick(); // в поле ввода пароль можно нажать Enter чтобы не нажимать на кнопку
         }
     }
 }
